a891b76c4e9b4f0c7633442436d81f63c58ce71c
Kyndall Sims <sims.kyndall22@gmail.com>
2019-10-07 16:55:12 -0500

    started with the exercise

---
+++ src/main/java/Student.java
@@ -0,0 +1,19 @@
+public class Student {
+
+    public long getId(){
+        return 12122121;
+    }
+
+    public static String getName(){
+        String name = "";
+        return name;
+    }
+
+    public void addGrade(int grade){
+        grade = 80;
+    }
+
+    public double getGradeAverage(){
+        return 80.3;
+    }
+}
---
+++ src/test/java/StudentTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class StudentTest {
+
+    @Test
+    public void ifItReturnsALong(){
+
+    }
+
+    @Test
+    public void ifItReturnsAString(){
+
+    }
+
+    @Test
+    public void ifItReturnsAInt(){
+
+    }
+
+    @Test
+    public void ifItReturnsADouble(){
+
+    }
+
+}
